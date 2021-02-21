c2fa90abf07993dd6e70c54c07507e66c5dfefc7
alex5388 <alexmiller5388@gmail.com>
2021-02-21 10:35:57 -0500

    Add files via upload

--- IS211_Assignment3v3.py
+++ IS211_Assignment3v3.py
@@ -3,41 +3,34 @@ import argparse
 import re
 from urllib import request
 
-url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
+url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv' #hardcoded URL
 
 fileOpen = request.urlopen(url)
 readFile = fileOpen.read()
-decFile = readFile.decode('ascii').split('\n')
-#for line in decFile:
-   #print(line)
+decFile = readFile.decode('ascii').split('\n')    #downloaded and parsed data
+
 
 x = re.findall("\.jpg|\.JPG", str(decFile))
 y = re.findall("\.gif|\.GIF", str(decFile))
-z = re.findall("\.png|\.PNG", str(decFile))
+z = re.findall("\.png|\.PNG", str(decFile))       #re used for searching
 
 m = re.findall("Mozilla", str(decFile))
 c = re.findall("Chrome", str(decFile))
 i = re.findall("Internet\sExplorer", str(decFile))
 s = re.findall("Safari", str(decFile))
 
-#print(len(m))
-#print(len(c))
-#print(i)
-#print(len(s))
+
 totalJPG = len(x)
 totalGIF = len(y)
 totalPNG = len(z)
-totalImg = len(x) + len(y) + len(z)
+totalImg = len(x) + len(y) + len(z)              #counts image links
 
-#print(f"Total .jpg images is {totalJPG}")
-#print(f"Total .gif images is {totalGIF}")
-#print(f"Total .png images is {totalPNG}")
 
 totalDecfile = len(decFile)
 div = totalImg/totalDecfile * 100
-#print("Image requests account for {:.1f}% of all requests".format(div))
 
-def Main():
+
+def Main():                                      #program options
     parser = argparse.ArgumentParser(description='Website Hits')
     parser.add_argument("-i", "--images", help="Show image requests", action='store_true')
     parser.add_argument("-j", "--jpg", help="Show number of .jpg image requests", action='store_true')
